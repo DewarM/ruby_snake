72d24474e5e99c0127dc7a5761256a84de3722f8
Mungo Dewar <mpjdewar@gmail.com>
2017-06-07 15:21:04 +0100

    Correct indentation

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -30,51 +30,51 @@ class GameTest < MiniTest::Test
     assert_equal(97, @snake.x_pos)
   end
 
-    def test_input_handler_deals_with_input_correctly__input_that_will_overlap_current_tail
-      @game.set_input(:up)
-      @game.move_snake()
-      assert_equal(51, @snake.y_pos)
-    end
+  def test_input_handler_deals_with_input_correctly__input_that_will_overlap_current_tail
+    @game.set_input(:up)
+    @game.move_snake()
+    assert_equal(51, @snake.y_pos)
+  end
 
-    def test_input_handler_cant_move_right_initially_as_tail_is_in_way
-      @game.set_input(:right)
-      @game.move_snake()
-      assert_equal(100, @snake.x_pos)
-    end
+  def test_input_handler_cant_move_right_initially_as_tail_is_in_way
+    @game.set_input(:right)
+    @game.move_snake()
+    assert_equal(100, @snake.x_pos)
+  end
 
-    def test_input_handler_deals_with_down_input
-      @game.set_input(:down)
-      @game.move_snake()
-      assert_equal(49, @snake.y_pos)
-    end
+  def test_input_handler_deals_with_down_input
+    @game.set_input(:down)
+    @game.move_snake()
+    assert_equal(49, @snake.y_pos)
+  end
 
-    def test_input_handler_deals_with_up_input
-      @game.set_input(:up)
-      @game.move_snake()
-      assert_equal(51, @snake.y_pos)
-    end
+  def test_input_handler_deals_with_up_input
+    @game.set_input(:up)
+    @game.move_snake()
+    assert_equal(51, @snake.y_pos)
+  end
 
-    def test_sequential_movement_no_new_input
-      @game.set_input(:up)
-      @game.move_snake()
-      @game.move_snake()
-      assert_equal(52, @snake.y_pos)
-    end
+  def test_sequential_movement_no_new_input
+    @game.set_input(:up)
+    @game.move_snake()
+    @game.move_snake()
+    assert_equal(52, @snake.y_pos)
+  end
 
-    def test_sequential_movement_with_new_input
-      @game.set_input(:up)
-      @game.move_snake()
-      @game.set_input(:right)
-      @game.move_snake()
-      assert_equal(51, @snake.y_pos)
-      assert_equal(101, @snake.x_pos)
-    end
+  def test_sequential_movement_with_new_input
+    @game.set_input(:up)
+    @game.move_snake()
+    @game.set_input(:right)
+    @game.move_snake()
+    assert_equal(51, @snake.y_pos)
+    assert_equal(101, @snake.x_pos)
+  end
 
-    def test_sequential_movement_with_new_input_invalid_new_input
-      @game.set_input(:up)
-      @game.move_snake()
-      @game.set_input(:down)
-      @game.move_snake()
-      assert_equal(51, @snake.y_pos)
-    end
+  def test_sequential_movement_with_new_input_invalid_new_input
+    @game.set_input(:up)
+    @game.move_snake()
+    @game.set_input(:down)
+    @game.move_snake()
+    assert_equal(51, @snake.y_pos)
+  end
 end
